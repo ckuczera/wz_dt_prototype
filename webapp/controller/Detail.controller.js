3f47593e38a3b2a4f12aeb8da18c6bcddd1c9207
Christoph Kuczera <39e79dc8-372a-4582-86f2-697d92ebfb38>
2023-05-10 10:15:11 +0000

    Detaiö Page

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,16 +1,49 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
 ],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("com.bmw.xss.ess.dt.wzdtprototype.controller.Detail", {
             onInit: function () {
                 var oRouter = this.getOwnerComponent().getRouter(this);
                 oRouter.getRoute("RouteDetail").attachPatternMatched(this._onObjectMatched, this);
+                this.getView().setModel(new JSONModel({
+                    "control": {
+                        "isEditable": false,
+                        "isSaveEnabled": false
+                    }
+                }), "viewModel")
+            },
+
+            onSavePressed: function(oEvent) {
+                var oModel = this.getView().getModel();
+                if(oModel.hasPendingChanges()) {
+                    oModel.submitChanges({
+                        success: function(oData) {
+                            debugger;
+                        },
+                        error: function(oData) {
+                            debugger;
+                        }
+                    });
+                }
+            },
+
+            onCancelPressed: function(oEvent) {
+                var oViewModel = this.getView().getModel("viewModel");
+                oViewModel.setProperty("/control/isEditable", !oViewModel.getProperty("/control/isEditable"));
+
+                var oModel = this.getView().getModel();
+                if(oModel.hasPendingChanges()) {
+                    oModel.resetChanges();
+                }
+            },
+
+            onEditPressed: function(oEvent) {
+                var oModel = this.getView().getModel("viewModel");
+                oModel.setProperty("/control/isEditable", !oModel.getProperty("/control/isEditable"));
             },
 
             _onObjectMatched: function(oEvent) {
